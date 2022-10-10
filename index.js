0d168757ad9ccf542c649beab05ea37f702cfe82
AlexStanislav <stanislav.alexandru@gmail.com>
2022-10-10 18:29:02 +0300

    Socket Test

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const app = require("./api/app")
+const { v4: uuidv4 } = require("uuid")
 
-app.listen(8080, () => {
-    console.log(`Server started on http://localhost:${8080}`);
+const server = app.listen(8000, () => {
+    console.log(`Server started on http://localhost:${8000}`);
+})
+const io = require("socket.io")(server, {
+    cors: {
+        origin: 'http://localhost:8080'
+    }
+})
+
+
+let ioConnections = []
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.id = uuidv4()
+    ioConnections[socket.id] = socket
+
+    socket.emit('connectionId', socket.id)
+
+    socket.on('disconnect', () => {
+        delete ioConnections[socket.id]
+        console.log('Client disconnected')
+    })
+    console.log(Object.keys(ioConnections));
 })
---
+++ public/js/about.d0815328.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"657a":function(n,t,u){"use strict";u.r(t);var a=function(){var n=this,t=n._self._c;return t("div")},e=[],l=u("2877"),o={},s=Object(l["a"])(o,a,e,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=about.d0815328.js.map
